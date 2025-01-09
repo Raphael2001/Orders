fc5718cd083f77dfbf81eb2e1fd58230573db706
Raphael <raphaelaboohi@gmail.com>
2025-01-09 21:29:52 +0200

    change api url

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -1,6 +1,6 @@
 const INACTIVE_USER_TIME_THRESHOLD = 6000000; // 60 min - a.k.a 1 hour
 const USER_ACTIVITY_THROTTLER_TIME = 60000;
-const BASE = "https://shipping360-cukp6uab4a-uc.a.run.app/api/v1";
+const BASE = "https://api-shipping360.aboohi.net/api/v1";
 const loginpageulr = "login.html";
 const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
 const just_to_target_list = ["Cheeta", "HFD"];
